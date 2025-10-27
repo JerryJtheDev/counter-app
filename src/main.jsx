ef7ff164e42bf1531f887ba9b7fb626aa6c48eae
JerryJunior <jerryjnr385@gmail.com>
2025-10-27 19:39:06 +0100

    Redux rocks

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//To provide our redux store to the entire react app we import it here
+import { Provider } from 'react-redux';
+import store from './store/index.js';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+  {/* We wrap our App component with the Provider component and pass the store as a prop */}
+  <Provider store={store}>
     <App />
+  </Provider>
   </StrictMode>,
 )
